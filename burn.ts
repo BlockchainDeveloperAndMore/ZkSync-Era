0232b3cee7b78e4c363b9ea4e45bd3f215750f34
BlockchainDeveloperAndMore <BlockchainDeveloperAndMore@gmail.com>
2023-05-14 21:06:55 +0400

    14.05.2023

--- burn.ts
+++ burn.ts
@@ -3,7 +3,7 @@ import * as ethers from "ethers";
 import * as flatted from 'flatted';
 import * as fs from 'fs';
 import { AccountsData } from "./Data/AccountsData";
-import { AmountGasLimit, TokenForLiquidity } from "./Data/TokensData";
+import { AmountGasLimit, TokenForLiquidity, HighGasPrice, timeMax, timeMin } from "./Data/TokensData";
 import { Contracts } from "./Data/Contracts";
 
 /*
@@ -31,16 +31,16 @@ function writeLog(log: any) {
  */
 
 // ZkSync Era Provider
-const zkSyncProvider = new zksync.Provider("https://testnet.era.zksync.dev");
+const zkSyncProvider = new zksync.Provider("https://mainnet.era.zksync.io");
 
 // Ethereum Provider
-const ethProvider = ethers.getDefaultProvider("goerli");
+const ethProvider = ethers.getDefaultProvider();
 
 /*
  * Burn liquidity function.
  */
 
-async function burnLiquidityTokenAndETH(signer: any, tokenAddress: string) {
+async function burnLiquidityTokenAndETH(signer: any, tokenAddress: string, AmountGasPrice: any) {
 
     let alreadyString: string = "Liquidity already burned!"
     let readyString: string = "Liquidity now burned!"
@@ -59,8 +59,6 @@ async function burnLiquidityTokenAndETH(signer: any, tokenAddress: string) {
     let ETHAddress = Contracts.wETH.Address;
     
     let ClassicPoolAddress: string = await classicPoolFactory.getPool(ETHAddress, tokenAddress);
-    writeLog("ClassicPoolAddress =");
-    writeLog(ClassicPoolAddress);
 
     // Checks whether the pool exists.
     if (ClassicPoolAddress === ethers.constants.AddressZero) {
@@ -73,8 +71,6 @@ async function burnLiquidityTokenAndETH(signer: any, tokenAddress: string) {
     // Check balance
     let balanceOf = await LPTokenInContract.balanceOf(signer.address)
     let LPTokensBalance: string = balanceOf.toString()
-    writeLog("LP token balance =");
-    writeLog(LPTokensBalance);
 
     if (Number(LPTokensBalance) == 0){
         return alreadyString
@@ -86,10 +82,20 @@ async function burnLiquidityTokenAndETH(signer: any, tokenAddress: string) {
     if (allowedAmount.lte(LPTokensBalance)) {
     
         // Send approve for router contract
-        let approveTx = await LPTokenInContract.approve(Contracts.RouterContract.Address, MAX_APPROVE_VALUE)
-        let getApproveTx = await approveTx.wait()
-        writeLog("Approve transaction hash =")
-        writeLog(getApproveTx.transactionHash);
+        let approveTx = await LPTokenInContract.approve(
+            Contracts.RouterContract.Address, 
+            MAX_APPROVE_VALUE,
+            {
+                gasPrice: AmountGasPrice,
+                gasLimit: AmountGasLimit
+            }
+        )
+
+        let getApproveTx = await approveTx.wait();
+
+        writeLog(`Approve transaction hash = ${getApproveTx.transactionHash}`)
+        console.log(`Approve transaction hash = ${getApproveTx.transactionHash}`);
+
     } 
 
     const withdrawMode = 1; // 1 or 2 to withdraw to user's wallet
@@ -99,8 +105,6 @@ async function burnLiquidityTokenAndETH(signer: any, tokenAddress: string) {
         ["address", "address", "uint8"],
         [ETHAddress, RecipientAddress, withdrawMode], // address _tokenOut address _to, uint8 _withdrawMode
     );
-    writeLog("RecipientAddressData =");
-    writeLog(RecipientAddressData);
 
     // Gets the router contract.
     const router = new ethers.Contract(
@@ -118,13 +122,14 @@ async function burnLiquidityTokenAndETH(signer: any, tokenAddress: string) {
         ethers.constants.AddressZero,   // we don't have a callback
         '0x',
         { 
+            gasPrice: AmountGasPrice,
             gasLimit: AmountGasLimit
         }
     );
 
     let tx = await response.wait();
-    writeLog("Burn liquidity transaction hash =");
-    writeLog(tx.transactionHash);
+    writeLog(`Burn liquidity transaction hash = ${tx.transactionHash}`);
+    console.log(`Burn liquidity transaction hash = ${tx.transactionHash}`);
 
     return readyString;
 }
@@ -141,24 +146,42 @@ async function burn(){
         
         // Creating a wallet
         const WorkingSigner = new zksync.Wallet(AccountsData[i], zkSyncProvider, ethProvider);
-        writeLog("Account now working =");
-        writeLog(WorkingSigner.address);        
+        writeLog(`Account now working = ${WorkingSigner.address}`);
+        console.log(`Account now working = ${WorkingSigner.address}`); 
+
+        // Check gas price
+        var AmountGasPrice = await zkSyncProvider.getGasPrice();
+        
+        if (AmountGasPrice.lt(HighGasPrice)){
 
         // Getting balance in ETH
         let AccountBalanceETH = await WorkingSigner.getBalance();
-        writeLog("Initial ETH balance =");
-        writeLog(AccountBalanceETH.toString());
+        writeLog(`Initial ETH balance = ${AccountBalanceETH.toString()}`);
+        console.log(`Initial ETH balance = ${AccountBalanceETH.toString()}`);
 
         // Burn
-        await burnLiquidityTokenAndETH(WorkingSigner, TokenForLiquidity)
+        await burnLiquidityTokenAndETH(WorkingSigner, TokenForLiquidity, AmountGasPrice)
 
         // Getting final balance in ETH
         let FinalAccountBalanceETH = await WorkingSigner.getBalance()
-        writeLog("Final ETH balance =");
-        writeLog(FinalAccountBalanceETH.toString());
-        writeLog("BURNING END");
+        writeLog(`Final ETH balance = ${ethers.utils.formatUnits(FinalAccountBalanceETH.toString(), 18)}`);
+        console.log(`Final ETH balance = ${ethers.utils.formatUnits(FinalAccountBalanceETH.toString(), 18)}`);
         writeLog("==============================================");
+        console.log("==============================================");
+        } else {
+            writeLog("GAS PRICE TO HIGH");
+            console.log("GAS PRICE TO HIGH");
+            writeLog("WAITING 5 MINS AND TRY AGAIN");
+            console.log("WAITING 5 MINS AND TRY AGAIN");
+            writeLog("==============================================");
+            console.log("==============================================");
+            setTimeout(burn, 300000)
+        }
     }
+
+    writeLog("BURNING END");
+    console.log("BURNING END");
+
 }
 
 burn();
